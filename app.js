7d3374094ca03ee98f5435a8eeb7a0ab9bb081d1
Maazul Haque <63408370+hmaazul@users.noreply.github.com>
2022-09-24 20:07:18 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,57 +1,47 @@
 const form = document.querySelector('#searchForm');
-const res = document.querySelector('#tableResult');
-var upd; 
-form.addEventListener('submit',(e)=>{
-
-    e.preventDefault();
-    if(upd){
+const res = document.querySelector('#result');
+var upd;
+form.addEventListener('submit',(f)=>{
+    f.preventDefault();
+    //check whether upd have some previous coin, then stop updating the previous coin//
+    if(upd)
+    {
         clearTimeout(upd);
     }
-
     const ctype = form.elements.coinType.value;
-
     fetchPrice(ctype);
+})
 
-});
+const fetchPrice = async(ctype) => {
+    const r = await axios.get(`https://api.coinstats.app/public/v1/coins/${ctype}?currency=USD`)
+    // console.log(r);
+    const name = r.data.coin.name;
+    const price = r.data.coin.price;
+    const vol = r.data.coin.volume;
+    const change = r.data.coin.priceChange1d;
+    const target = 'USD';
 
+    res.innerHTML = `
+    <tr style="background-color: blue; color: white; font-weight:700;">
+    <td style="border: 1px solid;">Property</td>
+    <td>Value</td>
+    </tr>
 
-const fetchPrice= async(ctype) =>{
-    const r = await axios.get(`https://api.coinstats.app/public/v1/coins/${ctype}?currency=USD`);
-    console.log(r.data.coin.price);
-     const price = r.data.coin.price;
-     const volume  = r.data.coin.volume;
-     const change = r.data.coin.priceChange1d;
-     const base = r.data.coin.name;
-     const target = 'USD';
-     
+    <tr>
+    <td>${name}</td>
+    <td>${price} ${target}</td>
+    </tr>
 
+    <tr>
+    <td>Volume</td>
+    <td>${vol}</td>
+    </tr>
 
-     res.innerHTML =`<tr style="background-color:red; color:white; font-weight:700">
-     <td>
-         Property
-     </td>
-     <td>Value</td>
- </tr>
- <tr>
-     <td>
-         ${base}
-     </td>
-     <td>${price} ${target}</td>
- </tr>
- <tr>
-     <td>
-         Volume
-     </td>
-     <td>${volume}</td>
- </tr>
- 
- <tr>
-     <td>
-         Change
-     </td>
-     <td>${change}</td>
- </tr>`
-
+    <tr>
+    <td>Change in 1-day</td>
+    <td>${change}</td>
+    </tr>
+    `
+    //update the current selected coin after every 10 seconds by calling the same function again //
     upd = setTimeout(()=>fetchPrice(ctype),10000);
-
 }
